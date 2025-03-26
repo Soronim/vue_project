0c9047794129ecebaecf76ad0576b476c525bc9e
БГУ <BSU@bktis.local>
2025-03-26 16:24:38 +0800

    Need_update

---
+++ stol/src/components/MyFooter.vue
@@ -0,0 +1,48 @@
+<template>
+    <footer id="footer" class="footer dark-background">
+        <div class="container">
+        <div class="row gy-3">
+            <div class="col-lg-3 col-md-6 d-flex">
+            <i class="bi bi-geo-alt icon"></i>
+            <div class="address">
+                <h4>Адрес</h4>
+                <p>Созвездие весов</p>
+                <p>Третья планета</p>
+                <p></p>
+            </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex">
+            <i class="bi bi-telephone icon"></i>
+            <div>
+                <h4>Контакты</h4>
+                <p>
+                <strong>Телефон:</strong> <span>+1 5589 55488 55</span><br>
+                <strong>Email:</strong> <span>food@brahium.com</span><br>
+                </p>
+            </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex">
+            <i class="bi bi-clock icon"></i>
+            <div>
+                <h4>Время работы</h4>
+                <p>
+                <strong>Пн-Пт:</strong> <span>11 - 24</span><br>
+                <strong>Сб-Вс</strong>: <span>10 - 01</span>
+                </p>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <div class="container copyright text-center mt-4">
+        <p>© <strong class="px-1 sitename">БГУ</strong> 2025</p>      
+        </div>
+    </footer>
+</template>
+<script>
+export default{
+    name:"MyFooter"
+}
+</script>
--- stol/src/main.js
+++ stol/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//Подключаю CSS
+import './assets/vendor/bootstrap/css/bootstrap-grid.css'
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+
+import '@/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '@/assets/vendor/glightbox/css/glightbox.min.css'
+import '@/assets/vendor/swiper/swiper-bundle.min.css'
+import '@/assets/css/main.css'
+
 createApp(App).mount('#app')
